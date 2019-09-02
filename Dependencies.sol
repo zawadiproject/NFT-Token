51daeacd34c7b49998ad0ed7ff682e266d3f7663
Steve Gachau <steve@zawadi.co>
2019-09-02 17:09:18 -0400

    Update Dependencies.sol

--- Dependencies.sol
+++ Dependencies.sol
@@ -75,18 +75,24 @@ library Counters {
 
 
 library Address {
-    
+    /**
+     * Returns whether the target address is a contract
+     * @dev This function will return false if invoked during the constructor of a contract,
+     * as the code is not actually created until after the constructor finishes.
+     * @param account address of the account to check
+     * @return whether the target address is a contract
+     */
     function isContract(address account) internal view returns (bool) {
-        
-        bytes32 codehash;
-        bytes32 accountHash = 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470;
+        uint256 size;
+        // XXX Currently there is no better way to check if there is a contract in an address
+        // than to check the size of the code at that address.
+        // See https://ethereum.stackexchange.com/a/14016/36603
+        // for more details about how this works.
+        // TODO Check this again before the Serenity release, because all addresses will be
+        // contracts then.
         // solhint-disable-next-line no-inline-assembly
-        assembly { codehash := extcodehash(account) }
-        return (codehash != 0x0 && codehash != accountHash);
-        
-    }
-    function toPayable(address account) internal pure returns (address payable) {
-        return address(uint160(account));
+        assembly { size := extcodesize(account) }
+        return size > 0;
     }
 }
 
